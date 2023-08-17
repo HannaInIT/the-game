118eae0e9710161d70b070dfbb9a3d65717819a9
HannaInIT <ann10062016@gmail.com>
2023-08-17 17:16:53 +0200

    collision detection, implemented the fall action for the player, fixed removal of obstacles

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 class Game {
     constructor (){
-        this.player = new Player(0, 0, 100, 100);
+        this.player = new Player(10, 30, 100, 100);
         this.obstaclesArr = []
     }
     start (){
@@ -12,10 +12,10 @@ class Game {
         }, 5000);
 
         setInterval(() => {
-            this.obstaclesArr.forEach((obstacleInstance) => {
+            this.obstaclesArr.forEach((obstacleInstance, i) => {
                 obstacleInstance.move();
                 this.detectCollision(obstacleInstance);
-                this.removeObstacleIfOutside(obstacleInstance);
+                this.removeObstacleIfOutside(obstacleInstance, i);
             });
         }, 10);
     }
@@ -23,32 +23,32 @@ class Game {
         document.addEventListener("keydown", (event) => {
             console.log(event.key)
             if (event.key === " ") {
+                if(!this.player.isJumping)
                 this.player.jump();
-            } else if (event.key === "ArrowRight") {
-                this.player.moveRight();
-            }
+                this.player.isJumping = true
+               
+            } 
         });
     }
-
-    removeObstacleIfOutside(obstacleInstance) {
+    removeObstacleIfOutside(obstacleInstance, i) {
+        
         const gameWidth = document.querySelector(".game").clientWidth;
-    
-        if (obstacleInstance.positionX > gameWidth) {
-            obstacleInstance.domElement.remove();
-            const index = this.obstaclesArr.indexOf(obstacleInstance);
-            if (index > -1) {
-                this.obstaclesArr.splice(index, 1);
-            }
+        if (obstacleInstance.positionX < -100) {
+            obstacleInstance.domElement.remove() ;
+            this.obstaclesArr.splice(i, 1)
         }
     }
      
     detectCollision(obstacleInstance) {
-        const playerTop = this.player.positionY;
-        const playerBottom = this.player.positionY + this.player.height;
-        const obstacleLeft = obstacleInstance.positionX;
+        // const playerTop = this.player.positionY;
+        // const playerBottom = this.player.positionY + this.player.height;
+        // const obstacleLeft = obstacleInstance.positionX;
         //const obstacleRight = obstacleInstance.positionX + obstacleInstance.width;
     
-        if (playerBottom > obstacleInstance.positionY && playerTop < obstacleInstance.positionY + obstacleInstance.height && obstacleInstance.positionX > 0) {
+        if ( this.player.x < obstacleInstance.positionX + obstacleInstance.width &&
+            this.player.x + this.player.width > obstacleInstance.positionX &&
+            this.player.y < obstacleInstance.positionY + obstacleInstance.height &&
+            this.player.y + this.player.height > obstacleInstance.positionY) {
             console.log("game over");
             location.href = "./gameover.html";
         }
@@ -64,6 +64,8 @@ class Player{
         this.speedY = 0
         this.gravity = 0.05
         this.domElement = null
+        this.initialY = y
+        this.isJumping = false
 
         this.createDomElement()
     }
@@ -75,20 +77,40 @@ class Player{
         this.domElement.style.position = "absolute"
         this.domElement.style.width = this.width + "px";
         this.domElement.style.height = this.height + "px";
-        this.domElement.style.left = this.x 
-        this.domElement.style.bottom = this.y 
+        this.domElement.style.left = this.x + "px"
+        this.domElement.style.bottom = this.y + "px"
 
         const parentElm = document.querySelector(".game");
         parentElm.appendChild(this.domElement);
     }
     jump(){
-        this.speedY = 10
-        this.y = this.y + this.speedY
-        this.domElement.style.bottom = this.y + "px"
+        this.speedY = 4
+        let jumpInterval = setInterval(()=> {
+            this.y = this.y + this.speedY
+            this.domElement.style.bottom = this.y + "px"
+
+        },10)
+        setTimeout(() => {
+            clearInterval(jumpInterval)
+            this.fall()
+        }, 1000)
     }
     fall(){
-        this.speedY = this.speedY - this.gravity
-        this.y = this.y + this.speedY
+        this.speedY = 4
+        let fallInterval = setInterval(() => {
+
+           
+            this.y = this.y - this.speedY
+            this.domElement.style.bottom = this.y + "px"
+        }, 10)
+        setTimeout(() => {
+            clearInterval(fallInterval)
+            this.isJumping = false
+        }, 1000)
+    }
+    resetPosition() {
+        this.y = this.initialY
+        this.domElement.style.bottom = this.y + "px"
     }
     }
 
@@ -97,7 +119,7 @@ class Obstacle {
         this.width = 10;
         this.height = 30;
         this.positionX = 1400;
-        this.positionY = 0;
+        this.positionY = 30;
         this.domElement = null;
 
         this.createDomElement();
@@ -112,7 +134,7 @@ class Obstacle {
         this.domElement.style.width = this.width + "vw";
         this.domElement.style.height = this.height + "vh";
         this.domElement.style.left = this.positionX + "px"
-        this.domElement.style.bottom = this.positionY 
+        this.domElement.style.bottom = this.positionY + "px"
 
         const parentElm = document.querySelector(".game");
         parentElm.appendChild(this.domElement);
